dd29f032597b4a6de56f1c4fa3e351e090db0500
taekhun <nth9708@naver.com>
2021-03-11 18:24:55 +0900

    Feature: selectLoginPage Done2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import MainPage from "./pages/mainPage/mainPage";
 import SelectLocationPage from "./pages/selectLocationPage/selectLocation";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import UsersRoutePage from "./pages/usersRoutePage/usersRoutePage";
-import HotplacesPage from "./pages/hotplacesPage/hotplacePages";
+import HotplacePage from "./pages/hotplacePage/hotplacePages";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Route exact path="/" component={MainPage} />
         <Route path="/start" component={SelectLocationPage} />
-        <Route exact path="/:id/places" component={HotplacesPage} />
+        <Route exact path="/:id/places" component={HotplacePage} />
         <Route exact path="/:id/routes" component={UsersRoutePage} />
       </Router>
     </div>
--- frontend/src/pages/hotplacePage/hotplacePages.jsx
+++ frontend/src/pages/hotplacePage/hotplacePages.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const HotplacePages = ({match}) => {
     const location = match.params.id;
-
     return (
         <div>
             <h1>{location} 핫플레이스</h1>
