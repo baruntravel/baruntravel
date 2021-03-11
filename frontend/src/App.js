9b65d80a968903ff441390f2b0f6c61798ae8d26
taekhun <nth9708@naver.com>
2021-03-11 16:31:01 +0900

    Feature : selectLocationPage 레이아웃 세팅

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import MainPage from "./pages/mainPage/mainPage";
+import SelectLocationPage from "./pages/selectLocationPage/selectLocation";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <Router>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/start" component={SelectLocationPage} />
+      </Router>
     </div>
   );
 }
