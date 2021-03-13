fb49fc0ad81f94b4ea6ee70308955c1d33a58c03
labyu <labyu2020@gmail.com>
2021-03-13 21:07:37 +0900

    Feature : FileServer 사용하지 않는 Import 제거

--- backend/api/src/main/java/me/travelplan/service/file/FileServer.java
+++ backend/api/src/main/java/me/travelplan/service/file/FileServer.java
@@ -1,11 +1,5 @@
 package me.travelplan.service.file;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-
 public enum FileServer {
     DISK,
     S3,
