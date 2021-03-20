58e5b8ec89be69101fed103306c6ae31e881299d
labyu <labyu2020@gmail.com>
2021-03-20 19:44:19 +0900

    Feature : Jenkins Pipeline Test4

--- backend/api/src/main/java/me/travelplan/ApiApplication.java
+++ backend/api/src/main/java/me/travelplan/ApiApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-
     }
 }
