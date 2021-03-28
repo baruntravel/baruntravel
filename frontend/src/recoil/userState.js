cc1a79c97a11daae208292d00306a8893e760235
ri2377@naver.com <ri2377@naver.com>
2021-03-28 16:22:18 +0900

    Feature : add Login & Register API and UserState update

--- frontend/src/recoil/userState.js
+++ frontend/src/recoil/userState.js
@@ -3,7 +3,9 @@ import { atom } from "recoil";
 export const userState = atom({
   key: "userState",
   default: {
-    isLogin: null, // 내 정보 -> 내id, 내가작성한 route Cart 등 나의 데이터 id들만 가져올 것들?
+    isLogin: null,
+    name: null, // 내 정보 -> 내id, 내가작성한 route Cart 등 나의 데이터 id들만 가져올 것들?
+    email: null,
     // me에 id들을 저장하면 리스트 중에서 내것이 있는 지 찾아보기 쉽다.
     userInfo: null,
   },
