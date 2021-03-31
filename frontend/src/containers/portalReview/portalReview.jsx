6ac5c0d1a2bf229cd3cd97dc3fad9a4960eec154
KwonGiSeok <ri2377@naver.com>
2021-03-31 21:02:19 +0900

    Feature : compose review Form & List

--- frontend/src/containers/portalReview/portalReview.jsx
+++ frontend/src/containers/portalReview/portalReview.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import ReviewForm from "../../components/reviewComponents/reviewForm/reviewForm";
 import ReviewList from "../../components/reviewComponents/reviewList/reviewList";
 import styles from "./portalReview.module.css";
 
 const PortalReview = ({ item, closeReviewList }) => {
+  const [writeReview, setWriteReview] = useState(true);
+  const writeReviewOpen = () => {
+    setWriteReview(true);
+  };
+  const writeReviewClose = () => {
+    setWriteReview(false);
+  };
   return (
     <div className={styles.PortalReview}>
-      <div className={styles.reviewList}>
-        <ReviewList item={item} closeList={closeReviewList} />
+      <div className={styles.reviewBody}>
+        {writeReview ? (
+          <ReviewForm
+            item={item}
+            onCloseReviewForm={writeReviewClose}
+            onClose={closeReviewList}
+          />
+        ) : (
+          <ReviewList
+            item={item}
+            closeList={closeReviewList}
+            writeReviewOpen={writeReviewOpen}
+          />
+        )}
       </div>
     </div>
   );
