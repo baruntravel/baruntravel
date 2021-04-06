a96667dd533e77434d0b1135a25f035856038129
KwonGiSeok <ri2377@naver.com>
2021-04-06 14:56:39 +0900

    Feature : place search bar

--- frontend/src/pages/hotplacePage/hotplacePage.jsx
+++ frontend/src/pages/hotplacePage/hotplacePage.jsx
@@ -7,6 +7,7 @@ import { ShoppingCartOutlined } from "@ant-design/icons";
 import { Drawer } from "antd";
 import ShoppingCart from "../../components/common/shoppingCart/shoppingCart";
 import DeleteConfirm from "../../components/common/deleteConfirm/deleteConfirm";
+import CategoryBar from "../../components/map/hotplaceMap/categoryBar/categoryBar";
 
 const HotplacePage = () => {
   const [cartVisible, setCartVisible] = useState(false);
@@ -28,39 +29,60 @@ const HotplacePage = () => {
     //
     console.log("삭제");
   };
-  {
-    // const [cartPortal, setCartPortal] = useState(false);
-    // const [place, setPlace] = useState({});
-    // const [inputKeyword, handleInputKeyword] = useInput();
-    // const [searchPlace, setSearchPlace] = useState("");
-    // const placeListRef = useRef();
-    // const handleCartPortalClose = useCallback(() => {
-    //   setCartPortal(false);
-    // }, []);
-    // const handleCartPortalOpen = useCallback(() => {
-    //   setCartPortal(true);
-    // }, []);
-    // const clickedPlace = useCallback((place) => {
-    //   setPlace(place);
-    // }, []);
-    // const handleSubmit = useCallback(
-    //   (e) => {
-    //     e.preventDefault();
-    //     if (inputKeyword === searchPlace) {
-    //       setSearchPlace(inputKeyword + " ");
-    //     } else {
-    //       setSearchPlace(inputKeyword);
-    //     }
-    //   },
-    //   [inputKeyword, searchPlace]
-    // );
-  }
+  const [cartPortal, setCartPortal] = useState(false);
+  const [place, setPlace] = useState({});
+  const [inputKeyword, handleInputKeyword] = useInput();
+  const [searchPlace, setSearchPlace] = useState("");
+  const placeListRef = useRef();
+  const handleCartPortalClose = useCallback(() => {
+    setCartPortal(false);
+  }, []);
+  const handleCartPortalOpen = useCallback(() => {
+    setCartPortal(true);
+  }, []);
+  const clickedPlace = useCallback((place) => {
+    setPlace(place);
+  }, []);
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (inputKeyword === searchPlace) {
+        setSearchPlace(inputKeyword + " ");
+      } else {
+        setSearchPlace(inputKeyword);
+      }
+    },
+    [inputKeyword, searchPlace]
+  );
   return (
     <div className={styles.HotplacePage}>
-      <button onClick={setConfirmPortalTrue}>test</button>
-      <button className={styles.cartOpenBtn} onClick={setCartVisibleTrue}>
-        <ShoppingCartOutlined />
-      </button>
+      <div className={styles.searchContainer}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            className={styles.inputBar}
+            placeholder="Search place..."
+            onChange={handleInputKeyword}
+            value={inputKeyword || ""}
+          />
+        </form>
+        <div className={styles.toggle}>
+          <ShoppingCartOutlined
+            className={styles.cartIcon}
+            onClick={setCartVisibleTrue}
+          />
+        </div>
+      </div>
+      <div className={styles.mapContainer}>
+        <HotplaceMap
+          placeListRef={placeListRef}
+          handleCartPortalOpen={handleCartPortalOpen}
+          clickedPlace={clickedPlace}
+          searchPlace={searchPlace}
+        />
+      </div>
+      <div className={styles.categoryContainer}>
+        <CategoryBar />
+      </div>
       <Drawer
         title={`${"장소"}의 담은 목록`}
         placement="right"
@@ -72,6 +94,7 @@ const HotplacePage = () => {
           backgroundColor: "#ebecec",
           padding: 0,
         }}
+        zIndex={1004}
       >
         <ShoppingCart setConfirmPortalTrue={setConfirmPortalTrue} />
       </Drawer>
@@ -81,38 +104,12 @@ const HotplacePage = () => {
           onClose={setConfirmPortalFalse}
         />
       )}
-      {/* <header className={styles.navbar}>navbar</header>
-      <div className={styles.body}>
-        <div className={styles.searchContainer}>
-          <div className={styles.search}>
-            <form className={styles.form} onSubmit={handleSubmit}>
-              <input
-                placeholder="Search place..."
-                onChange={handleInputKeyword}
-                value={inputKeyword || ""}
-              />
-              <button type="submit">검색</button>
-            </form>
-          </div>
-          <div className={styles.searchList}>
-            <ul ref={placeListRef} className={styles.placeList}></ul>
-          </div>
-        </div>
-        <div className={styles.mapContainer}>
-          <HotplaceMap
-            placeListRef={placeListRef}
-            handleCartPortalOpen={handleCartPortalOpen}
-            clickedPlace={clickedPlace}
-            searchPlace={searchPlace}
-          />
-        </div>
-      </div>
       {cartPortal && (
         <PortalCart
           place={place}
           handleCartPortalClose={handleCartPortalClose}
         />
-      )} */}
+      )}
     </div>
   );
 };
