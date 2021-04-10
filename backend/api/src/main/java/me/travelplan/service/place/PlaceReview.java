20d87448b6fc572e42681c24b1e97b94adb72994
labyu <labyu2020@gmail.com>
2021-04-10 14:19:40 +0900

    Feature : PlaceReview 엔티티 및 스키마 생성

---
+++ backend/api/src/main/java/me/travelplan/service/place/PlaceReview.java
@@ -0,0 +1,24 @@
+package me.travelplan.service.place;
+
+import lombok.*;
+import me.travelplan.config.jpa.BaseEntity;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "places_reviews")
+public class PlaceReview extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    private Double score;
+    private String content;
+}
