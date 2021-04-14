7a58650f20c53174796915587460fb2cd7ca7580
ri2377@naver.com <ri2377@naver.com>
2021-04-14 13:30:59 +0900

    Fix : routeDetail Page made

---
+++ frontend/src/pages/routeDetailPage/routeDetailPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./routeDetailPage.module.css";
+
+const RouteDetailPage = (props) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const images = [
+    "https://i.pinimg.com/236x/1a/3f/63/1a3f63dacc72b9ce7a0f41d771068c48.jpg",
+    "https://i.pinimg.com/236x/f0/8d/5b/f08d5b455ae78f7c89b99b0354a49b7d.jpg",
+    "https://i.pinimg.com/236x/1a/3f/63/1a3f63dacc72b9ce7a0f41d771068c48.jpg",
+    "https://i.pinimg.com/236x/f0/8d/5b/f08d5b455ae78f7c89b99b0354a49b7d.jpg",
+  ];
+  return (
+    <div className={styles.RouteDetailPage}>
+      <div className={styles.carouselContainer}>
+        <Slider {...settings}></Slider>
+      </div>
+    </div>
+  );
+};
+
+export default RouteDetailPage;
