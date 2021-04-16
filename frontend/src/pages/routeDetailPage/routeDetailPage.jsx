02099b2c7bbbee6c12a4e6031edddf66dd8ed951
KwonGiSeok <ri2377@naver.com>
2021-04-16 13:40:38 +0900

    Feature : add PlaceCard & ImageMap

--- frontend/src/pages/routeDetailPage/routeDetailPage.jsx
+++ frontend/src/pages/routeDetailPage/routeDetailPage.jsx
@@ -4,6 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./routeDetailPage.module.css";
 import DetailHeader from "../../components/detailHeader/detailHeader";
+import ImageMap from "../../components/map/imageMap/imageMap";
+import PlaceCard from "../../components/placeCard/placeCard";
 
 const RouteDetailPage = (props) => {
   const settings = {
@@ -19,6 +21,32 @@ const RouteDetailPage = (props) => {
     "https://i.pinimg.com/236x/1a/3f/63/1a3f63dacc72b9ce7a0f41d771068c48.jpg",
     "https://i.pinimg.com/236x/f0/8d/5b/f08d5b455ae78f7c89b99b0354a49b7d.jpg",
   ];
+  const places = [
+    {
+      id: "688578118",
+      place_name: "BNK저축은행 리테일금융센터",
+      place_url: "http://place.map.kakao.com/688578118",
+      address_name: "서울 중구 무교로 6",
+      x: "126.97943787116054",
+      y: "37.56657026127022",
+    },
+    {
+      id: "508437738",
+      place_name: "신한은행 서울시청금융센터",
+      placeUrl: "http://place.map.kakao.com/508437738",
+      address_name: "서울 중구 세종대로 110",
+      x: "126.978244947578",
+      y: "37.5662231640346",
+    },
+    {
+      id: "7975883",
+      place_name: "신한은행 서울광장출장소",
+      placeUrl: "http://place.map.kakao.com/7975883",
+      address_name: "서울 중구 을지로 16",
+      x: "126.979476558519",
+      y: "37.5658314512941",
+    },
+  ];
   return (
     <div className={styles.RouteDetailPage}>
       <DetailHeader />
@@ -30,12 +58,26 @@ const RouteDetailPage = (props) => {
             </div>
           ))}
         </Slider>
-        <div className={styles.imageCounter}>
-          <span> 1 / 21 </span>
+        <div className={styles.slider__placeNameBox}>
+          <span className={styles.slider__placeName}> placeName </span>
         </div>
       </div>
       <div className={styles.titleBox}>
         <h2 className={styles.title}>#해시태그 #제목</h2>
+      </div>
+      <div className={styles.contentBox}>
+        <span className={styles.content}>이 장소를 다녀왔어요</span>
+      </div>
+      <div className={styles.mapImage}>
+        <ImageMap />
+      </div>
+      <div className={styles.placesBox}>
+        {places &&
+          places.map((place, index) => (
+            <div key={index} className={styles.placeBox}>
+              <PlaceCard place={place} />
+            </div>
+          ))}
       </div>
     </div>
   );
