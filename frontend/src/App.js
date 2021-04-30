b7f64b32f0d246f0ebe9f50121dc8c2accff8c32
KwonGiSeok <ri2377@naver.com>
2021-04-30 18:43:09 +0900

    Feature : Profile case rendering & Logout component & function Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,73 +8,6 @@ import { RecoilRoot } from "recoil";
 import MyRoutePage from "./pages/myRoutePage/myRoutePage";
 import RouteDetailPage from "./pages/routeDetailPage/routeDetailPage";
 import PlaceDetailPage from "./pages/placeDetailPage/placeDetailPage";
-<<<<<<< HEAD
-import AllRoute from "./pages/selectContainer/selectRoute/allRoute/allRoute";
-import AllArea from "./pages/selectContainer/selectArea/allArea/allArea";
-import SideMyProfile from "./components/sideMyProfile/sideMyProfile";
-import { Drawer } from "antd";
-import ProfileEdit from "./components/profileEdit/profileEdit";
-
-function App() {
-  return (
-    <RecoilRoot>
-      <Router>
-        <Route exact path="/">
-          <MainPage />
-        </Route>
-        <Route path="/start">
-          <SelectContainer />
-        </Route>
-
-        <Route exact path="/:id/places">
-          <HotplacePage />
-        </Route>
-        <Route exact path="/all-place">
-          <AllArea />
-        </Route>
-
-        <Route exact path="/routes">
-          <UsersRoutePage />
-        </Route>
-        <Route exact path="/all-route">
-          <AllRoute />
-        </Route>
-        <Route exact path="/myRoute">
-          <MyRoutePage />
-        </Route>
-        <Route path="/routes/:id">
-          <RouteDetailPage />
-        </Route>
-
-        <Route exact path="/:id/place/detail">
-          <PlaceDetailPage />
-        </Route>
-
-        {/* 테스트 */}
-        <Route exact path="/test">
-          <Drawer
-            placement="right"
-            closable={true}
-            visible={true}
-            width={window.innerWidth > 768 ? "36vw" : "100vw"}
-            bodyStyle={{ padding: 0 }}
-          >
-            <ProfileEdit />
-          </Drawer>
-
-          {/* <Drawer
-            placement="right"
-            closable={true}
-            visible={true}
-            width={window.innerWidth > 768 ? "36vw" : "80vw"}
-            bodyStyle={{ padding: 0 }}
-          >
-            <SideMyProfile />
-          </Drawer> */}
-        </Route>
-      </Router>
-    </RecoilRoot>
-=======
 import AllRoute from "./pages/mainPage/selectRoute/allRoute/allRoute";
 import AllArea from "./pages/mainPage/selectArea/allArea/allArea";
 
@@ -126,7 +59,6 @@ function App() {
         </Router>
       </RecoilRoot>
     </div>
->>>>>>> feature/112
   );
 }
 
--- frontend/src/components/common/navbar/navbar.jsx
+++ frontend/src/components/common/navbar/navbar.jsx
@@ -1,8 +1,12 @@
 import styles from "./navbar.module.css";
 import Logo from "../logo/logo";
 import Menu from "../menu/menu";
+import { useState } from "react";
+import { Drawer } from "antd";
+import SideMyProfile from "../../sideMyProfile/sideMyProfile";
 
 const Navbar = () => {
+  const [openSideProfile, setOpenSideProfile] = useState(false);
   return (
     <>
       <div className={styles.navbarContainer}>
@@ -13,6 +17,15 @@ const Navbar = () => {
           <Menu />
         </div>
       </div>
+      <Drawer
+        placement="right"
+        closable={false}
+        visible={true}
+        width={window.innerWidth > 768 ? "36vw" : "80vw"}
+        bodyStyle={{ padding: 0 }}
+      >
+        <SideMyProfile />
+      </Drawer>
     </>
   );
 };
