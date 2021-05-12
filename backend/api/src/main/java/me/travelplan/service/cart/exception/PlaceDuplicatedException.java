04f79138ab55a31ce3a492e68c363f12509728a3
labyu <labyu2020@gmail.com>
2021-05-12 20:41:44 +0900

    Feature : Exception 이름 변경 및 상속 변경

--- backend/api/src/main/java/me/travelplan/service/cart/exception/PlaceDuplicatedException.java
+++ backend/api/src/main/java/me/travelplan/service/cart/exception/PlaceDuplicatedException.java
@@ -2,8 +2,8 @@ package me.travelplan.service.cart.exception;
 
 import me.travelplan.exception.BusinessException;
 
-public class PlaceExistedException extends BusinessException {
-    public PlaceExistedException() {
+public class PlaceDuplicatedException extends BusinessException {
+    public PlaceDuplicatedException() {
         super("카트에 이미 존재하는 장소입니다");
     }
 }
