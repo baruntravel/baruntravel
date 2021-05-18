9daffedb6e1b26bce1ea6fdbcd83b727dd04de29
KwonGiSeok <ri2377@naver.com>
2021-05-18 19:24:30 +0900

    Fix : sortBox color change

--- frontend/src/components/placeContentBox/placeContentBox.jsx
+++ frontend/src/components/placeContentBox/placeContentBox.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import SortBox from "../common/sortBox/sortBox";
+import styles from "./placeContentBox.module.css";
 
 const PlaceContentBox = ({ places }) => {
   // places : place id, place images
-  return <div>hi</div>;
+  return (
+    <div className={styles.PlaceContentBox}>
+      {/* <div className="functionBox"> */}
+      <div className={styles.functionBox}>
+        <span>아이콘 2개</span>
+        <SortBox />
+      </div>
+    </div>
+  );
 };
 
 export default PlaceContentBox;
