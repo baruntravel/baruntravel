2a4cf3ea467c601083024e688e2a7a21e97e895f
ri2377@naver.com <ri2377@naver.com>
2021-05-20 16:41:00 +0900

    Fix : reviewCard splitted to component

--- frontend/src/components/reviewComponents/reviewList/reviewCard/reviewCard.jsx
+++ frontend/src/components/reviewComponents/reviewList/reviewCard/reviewCard.jsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useState } from "react";
 import styles from "./reviewCard.module.css";
-import { Rate } from "antd";
 // import { getYear, getMonth, getDate } from "date-fns";
-import { LikeOutlined, LikeTwoTone } from "@ant-design/icons";
 import PostImages from "../../postImages/postImages";
 import ReviewUserProfile from "../../../common/reviewUserProfile/reviewUserProfile";
+import ReviewScoreText from "../../reviewScoreText/reviewScoreText";
+import ReviewCardBottom from "./reviewCardBottom/reviewCardBottom";
 
 const ReviewCard = ({
   review,
@@ -23,7 +23,6 @@ const ReviewCard = ({
   onOpenDeleteConfirm,
   onHandleSelected,
   onOpenEditForm,
-  isDetail,
 }) => {
   const [liked, setLiked] = useState(likeCheck); // post의 좋아요를 누른 사람들 중 유저가 있는 지 확인하는 작업,
   const onUnlike = () => {
@@ -47,58 +46,18 @@ const ReviewCard = ({
         onClickEdit={onClickEdit}
         onClickDelete={onClickDelete}
       />
-      <div className={styles.content}>
-        <div className={styles.info}>
-          <div className={styles.info__first}>
-            <Rate
-              className={styles.rate}
-              defaultValue={parseInt(score)}
-              disabled={true}
-              style={{
-                fontSize: "0.9em",
-                marginBottom: "12px",
-              }}
-            />
-            <span className={styles.score}>{parseInt(score)}</span>
-          </div>
-          <section>
-            <span className={styles.context}>{content}</span>
-          </section>
+      <ReviewScoreText content={content} score={score} />
+      {files.length > 0 && (
+        <div className={styles.imageContainer}>
+          <PostImages images={files.map((file) => file.url)} />
         </div>
-      </div>
-      {files.length > 0 &&
-        (isDetail ? (
-          files.map((file) => <img src={file.url} className={styles.image} />)
-        ) : (
-          <div className={styles.imageContainer}>
-            <PostImages images={files.map((file) => file.url)} />
-          </div>
-        ))}
-      <div className={styles.review__bottom}>
-        <div>
-          {liked ? (
-            <LikeTwoTone
-              style={{ padding: "0.5em" }}
-              twoToneColor="#eb2f96"
-              key="Like"
-              onClick={onUnlike}
-            />
-          ) : (
-            <LikeOutlined
-              style={{ padding: "0.5em" }}
-              key="Like"
-              onClick={onUnlike}
-            />
-          )}
-          <span className={styles.likeCount}>{likeCount}</span>
-        </div>
-        {/* <span className={styles.date}>{`${getYear(
-          createdAt || updatedAt
-        )}.${getMonth(createdAt || updatedAt)}.${getDate(
-          createdAt || updatedAt
-        )}`}</span> */}
-        <span className={styles.date}>{createdAt || updatedAt}</span>
-      </div>
+      )}
+      <ReviewCardBottom
+        liked={liked}
+        onUnlike={onUnlike}
+        date={updatedAt || createdAt}
+        likeCount={likeCount}
+      />
     </div>
   );
 };
