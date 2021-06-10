4b2ac1f13b81da7d815bddd0166f86da72ec28d8
KwonGiSeok <ri2377@naver.com>
2021-06-10 22:58:54 +0900

    Fix : wishPlaceCard image

--- frontend/src/components/routeMakerPage/wishPlaceCard/wishPlaceCard.jsx
+++ frontend/src/components/routeMakerPage/wishPlaceCard/wishPlaceCard.jsx
@@ -1,10 +1,8 @@
 import React, { useCallback } from "react";
 import styles from "./wishPlaceCard.module.css";
 
-const WishPlaceCard = ({ item, onAddCart }) => {
-  const index = 1;
-  const src =
-    "https://i.pinimg.com/474x/30/5a/21/305a216481dfaaec10fd59cf1f667652.jpg";
+const WishPlaceCard = ({ item, onAddCart, index }) => {
+  // const index = 1;
 
   const onAddCartFromCard = useCallback(() => {
     onAddCart(item);
@@ -12,13 +10,15 @@ const WishPlaceCard = ({ item, onAddCart }) => {
 
   return (
     <div className={styles.WishPlaceCard} onClick={onAddCartFromCard}>
-      <img className={styles.image} src={src} />
+      <img className={styles.image} src={item.image} />
       <div className={styles.nameBox}>
         <span className={styles.name}>{item.name}</span>
       </div>
-      <div className={styles.indexBox}>
-        <span className={styles.index}>{index}</span>
-      </div>
+      {index && (
+        <div className={styles.indexBox}>
+          <span className={styles.index}>{index}</span>
+        </div>
+      )}
     </div>
   );
 };
