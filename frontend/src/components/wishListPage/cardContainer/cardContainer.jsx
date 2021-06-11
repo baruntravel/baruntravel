bbd7ddeecc9b605d8e274b02afdf294b7c092e7f
Taekhun <nth9708@naver.com>
2021-06-11 14:40:33 +0900

    Feature: 카드 컨테이너 event handling

--- frontend/src/components/wishListPage/cardContainer/cardContainer.jsx
+++ frontend/src/components/wishListPage/cardContainer/cardContainer.jsx
@@ -5,29 +5,25 @@ const CardContainer = ({ folderIn }) => {
   const titleRef = useRef();
 
   const passTitle = (e) => {
-    // console.log(titleRef.current);
+    console.log(e.target);
     folderIn();
   };
 
   return (
     <div>
-      <div className={styles.cardsContainer}>
-        <div className={styles.cardBox} onClick={passTitle}>
-          {/* <div className={styles.imgBox}>
-            <div className={styles.img1}>img1</div>
+      <div className={styles.cardsContainer} onClick={passTitle}>
+        <div className={styles.cardBox}>
+          <div className={styles.imgBox}>
+            {/* <div className={styles.img1}>img1</div> */}
             <div className={styles.img2}>img2</div>
             <div className={styles.img3}>img3</div>
-          </div> */}
-          <span className={styles.wishListTitle} ref={titleRef}>
-            제주도 갈만한 곳들
-          </span>
+          </div>
+          <span className={styles.wishListTitle}>제주도 갈만한 곳들</span>
         </div>
 
-        <div className={styles.cardBox} onClick={passTitle}>
+        <div className={styles.cardBox}>
           <div className={styles.imgBox}></div>
-          <span className={styles.wishListTitle} ref={titleRef}>
-            서울 핫플레이스
-          </span>
+          <span className={styles.wishListTitle}>서울 핫플레이스</span>
         </div>
       </div>
     </div>
