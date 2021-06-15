20f01fc165b88173bf01ce0d2e75c1839ae5132f
ri2377@naver.com <ri2377@naver.com>
2021-06-15 15:31:23 +0900

    Fix : WishListPortal close animation when x button clicked

--- frontend/src/components/portal/wishListPortal/wishListPortal.jsx
+++ frontend/src/components/portal/wishListPortal/wishListPortal.jsx
@@ -1,22 +1,30 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import VerticalWishList from "../../common/verticalWishList/verticalWishList";
 import Portal from "../portal";
 import styles from "./wishListPortal.module.css";
 const WishListPortal = ({ onClose, onAddItem }) => {
   const componentRef = useRef();
   const portalRef = useRef();
-  const onHandleClose = (event) => {
-    if (
-      portalRef.current &&
-      !portalRef.current.contains(event.target) &&
-      event.target.nodeName !== "SPAN" // span을 클릭할 때 리렌더링되는 이유로 예외케이스 추가
-    ) {
-      portalRef.current.classList.add("slide-bottom-portal");
-      setTimeout(() => {
-        onClose();
-      }, 500);
-    }
-  };
+
+  const onCloseWithAnimation = useCallback(() => {
+    portalRef.current.classList.add("slide-bottom-portal");
+    setTimeout(() => {
+      onClose();
+    }, 500);
+  }, [onClose]);
+
+  const onHandleClose = useCallback(
+    (event) => {
+      if (
+        portalRef.current &&
+        !portalRef.current.contains(event.target) &&
+        event.target.nodeName !== "SPAN" // span을 클릭할 때 리렌더링되는 이유로 예외케이스 추가
+      ) {
+        onCloseWithAnimation();
+      }
+    },
+    [onCloseWithAnimation]
+  );
   useEffect(() => {
     componentRef.current.addEventListener("click", onHandleClose);
   }, [onHandleClose]);
@@ -25,7 +33,7 @@ const WishListPortal = ({ onClose, onAddItem }) => {
     <Portal>
       <div ref={componentRef} className={styles.WishListPortal}>
         <div ref={portalRef} className={styles.body}>
-          <VerticalWishList onClose={onClose} onAddItem={onAddItem} />
+          <VerticalWishList onClose={onCloseWithAnimation} onAddItem={onAddItem} />
         </div>
       </div>
     </Portal>
