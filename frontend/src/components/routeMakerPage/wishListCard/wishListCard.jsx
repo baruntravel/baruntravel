46397abf7925b75ca44619d92bbff36413418cae
ri2377@naver.com <ri2377@naver.com>
2021-06-17 15:29:59 +0900

    Fix : wishPlaceCard container fixed width => 33vw

--- frontend/src/components/routeMakerPage/wishListCard/wishListCard.jsx
+++ frontend/src/components/routeMakerPage/wishListCard/wishListCard.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import styles from "./wishListCard.module.css";
 
 const WishListCard = ({ item }) => {
-  const src =
-    "https://i.pinimg.com/564x/d7/ec/75/d7ec75c9e68873ee75b734ac4ab09ced.jpg";
+  const src = "https://i.pinimg.com/564x/d7/ec/75/d7ec75c9e68873ee75b734ac4ab09ced.jpg";
   return (
     <div className={styles.WishListCard}>
       <img className={styles.image} src={src} />
